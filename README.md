8ccb7debd2f40e2f4237437666d030734ed7f7ea
ZlatkoMarianov <zlatkomarianov@abv.bg>
2025-10-05 22:31:14 +0300

    Show edit and delete buttons only to movie creators

--- README.md
+++ README.md
@@ -143,7 +143,7 @@ ___
 - [x] Add edit and delete buttons on details page
 - [x] Add creator as relation to movies
 - [x] Add user as creator on movie create
-- [ ] Show buttons only for creators
+- [x] Show buttons only for creators
  
 
 ### Delete Movies
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -29,9 +29,10 @@ movieController.get('/:movieId/details', async (req, res) => {
    // TODO Prepare view data (temp solution)
    const ratingViewData = '&#x2605;'.repeat(Math.trunc(movie.rating));
 
-   // const  isCreator = 
+   // const isCreator = req.user?.id && movie.creator == req.user.id;
+   const isCreator = movie.creator && movie.creator.equals(req.user?.id);
 
-   res.render('movies/details', { movie, rating: ratingViewData, });
+   res.render('movies/details', { movie, rating: ratingViewData, isCreator });
 });
 
 movieController.get('/search', async (req, res) => {
